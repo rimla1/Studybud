da03583d35d4c1c985d1eb77e0d1d20ef9f98b87
Almir Muminovic <almirmuminovic@Almirs-MacBook-Pro.local>
2021-12-15 19:27:15 +0100

    Views and URLs

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path, include
 
-def home(request):
-    return HttpResponse("Home pageeee")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),
+    path('', include('base.urls')),
 ]
 
 
